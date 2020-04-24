db2b6a4f09c02054d6c2ef026993b67f5041d747
Rana Emad <rana3emad@gmail.com>
2020-04-24 13:11:37 +0200

    Updates

---
+++ precache-manifest.fe53959e2aa1711a0e2473335571b9c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f43e710dae334b26f918fe5fe6d685",
+    "url": "/woof/index.html"
+  },
+  {
+    "revision": "54b7017e7019cc82f158",
+    "url": "/woof/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba537a017e53af500ee6",
+    "url": "/woof/static/js/2.45fcb56c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/woof/static/js/2.45fcb56c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b7017e7019cc82f158",
+    "url": "/woof/static/js/main.25485f8c.chunk.js"
+  },
+  {
+    "revision": "7204d0f276bcc5cea98b",
+    "url": "/woof/static/js/runtime-main.a88791a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/woof/static/media/logo.5d5d9eef.svg"
+  }
+]);
